23c4b3f7dbc3caf47596f9707f75c3a57b1d7aef
phenriqued <112258290+phenriqued@users.noreply.github.com>
2025-10-15 11:28:03 -0300

    Initial Commit

---
+++ src/test/java/com/github/phenriqued/api_pagamento/ApiPagamentosMicrosservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phenriqued.api_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentosMicrosservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
